2dcb67b821512ee23138355ea90b7c7af74ba5ab
cd17822 <cdigiov1@binghamton.edu>
2015-03-17 17:36:19 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , fs = require('fs');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', function(req,res){
+        fs.readFile('./views/vavoom.html', function(error, content) {
+                if (error) {
+                        res.writeHead(500);
+                        res.end();
+                }
+                else {
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.end(content, 'utf-8');
+                }
+        });
+});
+
+app.listen(80, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
