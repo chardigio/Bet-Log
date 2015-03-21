38a18f4e12b7d0c297695eb972485a5c92fe2b6f
cd17822 <cdigiov1@binghamton.edu>
2015-03-21 17:04:36 -0400

    front end first page init commit

---
+++ public/javascripts/gamblinghome.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+        $("#create").click(function(){
+               alert("working") var html = "<p>Group name</p>
+<input type="text" name="groupname"/>";
+                $(this).append(html);
+        });                                     
+});
