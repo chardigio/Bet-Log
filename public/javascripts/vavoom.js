21d0a9ad961e7b65b3dc1dc921fdf1a2b1d978ae
Charles DiGiovanna <cdigiov1@binghamton.edu>
2015-05-07 03:07:10 -0400

    more more more cleaning

--- public/javascripts/vavoom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-   $(".jumper").click(function(e){
-	e.preventDefault();
-
-        $("body, html").animate({ 
-            scrollTop: $( $(this).attr('href') ).offset().top
-        }, 600);
-    });
-});
